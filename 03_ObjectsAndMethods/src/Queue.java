2b9d460f16b68cbb62cdbbf9668aec4ebf4ff500
Aleksei Vorobyev <alekseivorobyevmanager@gmail.com>
2021-05-10 18:43:52 +0300

    queue 3

--- 03_ObjectsAndMethods/src/Queue.java
+++ 03_ObjectsAndMethods/src/Queue.java
@@ -1,32 +1,27 @@
+import java.util.Arrays;
+
 public class Queue
 {
  public static int countQueue = 0;
  static String buyers = " ";
- public static void increaseCount(int count) {
-  Queue.countQueue = Queue.countQueue + count;
- }
- public Queue() {
-  increaseCount(1);
-  buyers = "Список покупателей:";
- }
  public void addQueue(String name){
-  countQueue++;
+
  }
  public void addQueue(String name, int numberPhone){addQueue(name);
-  countQueue++;
+
  }
  public void addQueue(String name, int numberPhone, String iMail){addQueue(name, numberPhone);
-  countQueue++;
-  buyers = buyers + countQueue + "\n" + name + ", " + "Номер телефона: " + numberPhone
-           + "Электронная почта" + iMail;
+
+  buyers = buyers + name + ", " + "Номер телефона: " + numberPhone + ", Электронная почта " + iMail + "\n";
  }
- public void flush(){
-  if(countQueue <= 10) {
-   System.out.println(countQueue);
+ public void print() {
+  if (buyers.isEmpty()) {
+   System.out.println("Очередь пуста");
+  } else {
+
+   System.out.println(buyers);
   }
-  else {
-   return;
-  }
+
 
   }
  }
